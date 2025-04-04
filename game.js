0b461529730f59e5265090aa07ee09d044c57f1a
Reuben <reuben534@gmail.com>
2025-04-04 21:54:50 +0200

    Add Codey sprite with preload and create

--- game.js
+++ game.js
@@ -1,7 +1,16 @@
+function preload() {
+    this.load.image('codey', 'https://content.codecademy.com/courses/learn-phaser/codey.png');
+}
+
 function create() {
     this.add.text(50, 100, "Ruby's Adventures\n  in Showdown", { font: "40px Times New Roman", fill: "#ffa0d0" });
     this.add.text(130, 300, "by Reuben", { font: "20px Times New Roman", fill: "#ffa0d0" });
-    this.add.circle(200, 400, 30, 0x00FF00);
+    this.add.circle(100, 200, 20, 0xFF0000);
+    this.add.circle(200, 200, 20, 0x00FF00);
+    this.add.circle(300, 200, 20, 0x0000FF);
+    this.add.circle(400, 200, 20, 0xFFFF00);
+    let circle5 = this.add.circle(150, 400, 25, 0xFF00FF);
+    this.add.sprite(225, 450, 'codey');
 }
 
 const config = {
@@ -10,6 +19,7 @@ const config = {
     height: 600,
     backgroundColor: "#5f2a55",
     scene: {
+        preload,
         create
     }
 };
