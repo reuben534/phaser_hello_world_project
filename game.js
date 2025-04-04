e41e9893dbb091eee4b91d2fcd50d32a8032731b
Reuben <reuben534@gmail.com>
2025-04-04 21:34:47 +0200

    Create Phaser title screen with custom title and name

---
+++ game.js
@@ -0,0 +1,16 @@
+function create() {
+    this.add.text(50, 100, "Ruby's Adventures\n  in Showdown", { font: "40px Times New Roman", fill: "#ffa0d0" });
+    this.add.text(130, 300, "by Reuben", { font: "20px Times New Roman", fill: "#ffa0d0" });
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 450,
+    height: 600,
+    backgroundColor: "#5f2a55",
+    scene: {
+        create
+    }
+};
+
+const game = new Phaser.Game(config);
