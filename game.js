c91fdb810d74c7cafb2d18c89a586e50ae054d48
Reuben <reuben534@gmail.com>
2025-04-04 21:45:47 +0200

    Add circle5 to Phaser title screen

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 function create() {
     this.add.text(50, 100, "Ruby's Adventures\n  in Showdown", { font: "40px Times New Roman", fill: "#ffa0d0" });
     this.add.text(130, 300, "by Reuben", { font: "20px Times New Roman", fill: "#ffa0d0" });
+    this.add.circle(200, 400, 30, 0x00FF00);
 }
 
 const config = {
